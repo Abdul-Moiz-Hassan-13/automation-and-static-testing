fcbc0f2b346b54eab1236ecacfa8f2e3998b2c0d
Abdul Moiz Hassan <124426213+Abdul-Moiz-Hassan-13@users.noreply.github.com>
2025-07-26 20:20:17 +0500

    Update invalid_login_spec.cy.js

--- Automation Scripts/invalid_login_spec.cy.js
+++ Automation Scripts/invalid_login_spec.cy.js
@@ -1,38 +1,18 @@
 describe('Invalid Login', () => {
-    it('should show an error message when login credentials are invalid', () => {
-      // Visit the login page
-      cy.visit('http://localhost:5173/login');
-  
-      // Clear any existing text in the email field and type an invalid email
-      cy.get('[type="text"]').clear().type('invalidemail@gmail.com');
-  
-      // Clear any existing text in the password field and type an invalid password
-      cy.get('[type="password"]').clear().type('wrongpassword');
-  
-      // Click the login button
-      cy.get('.ant-btn').click();
-  
-      // Ensure the page does not redirect to the home page
-      cy.url().should('eq', 'http://localhost:5173/login');
-  
-      // Check if an error message is displayed
-      cy.contains('Wrong').should('be.visible');
-    });
-  
-    it('should fail to login with empty fields', () => {
-      // Visit the login page
-      cy.visit('http://localhost:5173/login');
-  
-      // Clear the email and password fields to simulate empty input
-      cy.get('[type="text"]').clear();
-      cy.get('[type="password"]').clear();
-  
-      // Click the login button
-      cy.get('.ant-btn').click();
-  
-      // Ensure the page does not redirect to the home page
-      cy.url().should('eq', 'http://localhost:5173/login');
-  
-    });
+  it('should show an error message when login credentials are invalid', () => {
+    cy.visit('http://localhost:5173/login');
+    cy.get('[type="text"]').clear().type('invalidemail@gmail.com');
+    cy.get('[type="password"]').clear().type('wrongpassword');
+    cy.get('.ant-btn').click();
+    cy.url().should('eq', 'http://localhost:5173/login');
+    cy.contains('Wrong').should('be.visible');
   });
-  
+
+  it('should fail to login with empty fields', () => {
+    cy.visit('http://localhost:5173/login');
+    cy.get('[type="text"]').clear();
+    cy.get('[type="password"]').clear();
+    cy.get('.ant-btn').click();
+    cy.url().should('eq', 'http://localhost:5173/login');
+  });
+});
